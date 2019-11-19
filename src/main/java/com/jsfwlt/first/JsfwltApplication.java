c42c07ea1fb6382f0038b95b58f6648966b96229
hemycoo <1063854432@qq.com>
2019-11-19 16:04:29 +0800

    Initial commit

---
+++ src/main/java/com/jsfwlt/first/JsfwltApplication.java
@@ -0,0 +1,14 @@
+package com.jsfwlt.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsfwltApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsfwltApplication.class, args);
+        System.out.println("start success");
+    }
+
+}
