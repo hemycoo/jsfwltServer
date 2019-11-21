ea77ab9c13bcd1f3d11fa2c003937d13acd59f11
hemycoo <1063854432@qq.com>
2019-11-21 14:57:40 +0800

    更新VO

--- src/main/java/com/jsfwlt/first/controller/TestUrl1.java
+++ src/main/java/com/jsfwlt/first/controller/TestUrl1.java
@@ -2,8 +2,8 @@ package com.jsfwlt.first.controller;
 
 import com.jsfwlt.first.mapper.Test2Mapper;
 import com.jsfwlt.first.mapper.TestMapper;
-import com.jsfwlt.first.po.Test2Po;
 import com.jsfwlt.first.vo.TestVo1;
+import com.jsfwlt.first.vo.TestVo2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestUrl1 {
 
-    @Autowired
+    @Autowired(required = false)
     private TestMapper testMapper;
 
-    @Autowired
+    @Autowired(required = false)
     private Test2Mapper test2Mapper;
 
     @GetMapping("/test")
@@ -26,12 +26,11 @@ public class TestUrl1 {
         return testVo1;
     }
 
-    @GetMapping("/mabatis")
-    public Test2Po testMybatis(){
-        Test2Po test2Po = new Test2Po();
+    @GetMapping("/mybatis")
+    public TestVo2 testMybatis(){
+        TestVo2 test2Po = new TestVo2();
         test2Po.setName(test2Mapper.selectByPrimaryKey(1).getName());
         test2Po.setAge(test2Mapper.selectByPrimaryKey(1).getAge());
-        test2Po.setTid(1);
         System.out.println("test mybatis success");
         return test2Po;
     }
