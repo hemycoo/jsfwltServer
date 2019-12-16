a2ee915e04ab593db4db405964e6d33431537dc0
hemycoo <1063854432@qq.com>
2019-12-16 09:21:38 +0800

    删除TestVo1 2

--- src/main/java/com/jsfwlt/first/vo/TestVo1.java
+++
@@ -1,9 +0,0 @@
-package com.jsfwlt.first.vo;
-
-import lombok.Data;
-
-@Data
-public class TestVo1 {
-    private String name;
-    private String age;
-}
--- src/main/java/com/jsfwlt/first/vo/TestVo2.java
+++
@@ -1,9 +0,0 @@
-package com.jsfwlt.first.vo;
-
-import lombok.Data;
-
-@Data
-public class TestVo2 extends BaseVo{
-    private String name;
-    private String age;
-}
