93fb91dcc76b3be2654bb6397c39a5ab612d5615
hemycoo <1063854432@qq.com>
2020-01-10 16:50:52 +0800

    更改topicAnswer的dto

--- src/main/java/com/jsfwlt/first/api/roast/TopicAnswerApi.java
+++ src/main/java/com/jsfwlt/first/api/roast/TopicAnswerApi.java
@@ -7,7 +7,7 @@ import com.jsfwlt.first.po.roast.TopicAnswerPo;
 import com.jsfwlt.first.service.roast.TopicAnswerService;
 import com.jsfwlt.first.vo.roast.CommentReplyListResp;
 import com.jsfwlt.first.vo.roast.TopicAnswerListResp;
-import com.jsfwlt.first.dto.roast.TopicDetailDto;
+import com.jsfwlt.first.dto.roast.TopicAnswerDto;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -29,7 +29,7 @@ public class TopicAnswerApi extends BaseApi {
     private int queryNum = 0; //测试使用
 
     @GetMapping("/roast/topic/answer/query/{topicId}")
-    public TopicAnswerListResp queryTopicAnswer(@PathVariable("topicId") int req) throws Exception {
+    public TopicAnswerListResp queryTopicAnswer(@PathVariable("topicId") int req){
         System.out.println("req  : " + req);
         TopicAnswerListResp topicAnswerListResp = new TopicAnswerListResp();
         List<TopicAnswerPo> topicAnswerList = topicAnswerPoMapper.selectByTopicId(req);
@@ -38,22 +38,23 @@ public class TopicAnswerApi extends BaseApi {
         }
         System.out.println(date2String(topicAnswerList.get(0).getCreateTime()));
         for (TopicAnswerPo list : topicAnswerList){
-            TopicDetailDto topicDetailDto = new TopicDetailDto();
-            BeanUtils.copyProperties(list,topicDetailDto);
-            topicDetailDto.setCreationTime(date2String( list.getCreateTime()));
-            topicDetailDto.setModificationTime(date2String(list.getModifyTime()));
-            topicAnswerListResp.getTopicDetaildata().add(topicDetailDto);
+            TopicAnswerDto topicAnswerDto = new TopicAnswerDto();
+            BeanUtils.copyProperties(list, topicAnswerDto);
+            topicAnswerDto.setCreateTime(date2String( list.getCreateTime()));
+            topicAnswerDto.setModifyTime(date2String(list.getModifyTime()));
+            topicAnswerListResp.getTopicAnswerData().add(topicAnswerDto);
         }
         queryNum++;
-        System.out.println("query success" + queryNum);
+        System.out.println("query success  " + queryNum);
         return topicAnswerListResp;
     }
 
     @GetMapping("/roast/topic/answer/querycomment/{topicAnswerId}")
-    public CommentReplyListResp queryCommentReply(@PathVariable Integer topicAnswerId){
+    public CommentReplyListResp queryCommentReply(@PathVariable int topicAnswerId){
         CommentReplyListResp commentReplyListResp = topicAnswerService.selectCommentAndReplyByTopicAnswerId(topicAnswerId);
         System.out.println("comment success");
         return commentReplyListResp;
     }
+    
 
 }
